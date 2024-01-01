2d7d111aa2dd21a104fb4dd493572549ec5c3ede
takejohn <105504345+takejohn@users.noreply.github.com>
2024-01-02 00:57:32 +0900

    Add tests of Tuple#iterator

--- src/test/java/jp/takejohn/bask/util/TupleTest.java
+++ src/test/java/jp/takejohn/bask/util/TupleTest.java
@@ -3,9 +3,9 @@ package jp.takejohn.bask.util;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class TupleTest {
 
@@ -25,6 +25,18 @@ class TupleTest {
         assertEquals("(abc)", single.toString());
         assertEquals("(def, 123)", dbl.toString());
         assertEquals("(ghi, 456, 7.89)", triple.toString());
+
+        assertEquals(Tuple.of("ghi", 456, 7.89), triple);
+    }
+
+    @Test
+    void iterator() {
+        final var tuple = Tuple.of(false, 1, 3.14);
+        final var iterator = tuple.iterator();
+        assertEquals(false, iterator.next());
+        assertEquals(1, iterator.next());
+        assertEquals(3.14, iterator.next());
+        assertThrows(NoSuchElementException.class, iterator::next);
     }
 
 }
