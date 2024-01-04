f71d6c70d2ef176b0500c7085a7e2ec190ad3c54
takejohn <105504345+takejohn@users.noreply.github.com>
2024-01-05 00:50:32 +0900

    Create SkriptSyntaxProcessor

--- annotations/src/main/java/module-info.java
+++ annotations/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module Bask.annotations.main {
 
+    requires java.compiler;
     requires org.jetbrains.annotations;
     exports jp.takejohn.bask.annotations;
+    provides javax.annotation.processing.Processor with jp.takejohn.bask.annotations.SkriptSyntaxProcessor;
 
 }
