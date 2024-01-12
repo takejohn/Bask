42aad06d93ef16d80de5a49b859def33f8fe0911
takejohn <105504345+takejohn@users.noreply.github.com>
2024-01-13 00:21:26 +0900

    Add Bask#getInstance method

--- src/main/java/jp/takejohn/bask/Bask.java
+++ src/main/java/jp/takejohn/bask/Bask.java
@@ -6,10 +6,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Bask extends JavaPlugin {
 
+    private static Bask instance;
+
     @Override
     public void onEnable() {
+        setInstance(this);
         Skript.registerAddon(this);
         ClassRegistry.load();
+    }
+
+    private static void setInstance(Bask instance) {
+        Bask.instance = instance;
+    }
+
+    public static Bask getInstance() {
+        return instance;
     }
 
 }
